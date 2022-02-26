e7777d3f77bfac9cba80cdd3e62ee647507fea7c
Jen Looper <jen.looper@gmail.com>
2022-02-26 11:07:20 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,47 @@
+def on_button_pressed_a():
+    for index in range(3):
+        basic.show_leds("""
+            . . . . .
+                        . # # # .
+                        . # # # .
+                        . # # # .
+                        . . . . .
+        """)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    for index2 in range(3):
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . # # # .
+                        . . . . .
+                        . . . . .
+        """)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    for index3 in range(3):
+        basic.show_leds("""
+            . . . . .
+                        . # # # .
+                        . # # # .
+                        . # # # .
+                        . . . . .
+        """)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
